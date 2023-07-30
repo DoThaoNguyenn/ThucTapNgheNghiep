a56414b8cd28e474553469526f9f2802fcc89dfe
minhpttk <dinhminh060102@gmail.com>
2023-07-30 16:58:52 +0700

    css login-register1

--- demoweb/product/views.py
+++ demoweb/product/views.py
@@ -6,14 +6,13 @@ from order.models import Order, Users, Order_detail
 from .forms import Product_create_form, Category_create_form, Register_form, Add_Product_information,UserInformationForm, OrderForm
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
+from django.db.models import Q
+
 
 # Create your views here.
 def index(request):
     ds = Category.objects.all()
-    sp = Product.objects.all()[:3]
-   
-    print(sp)
-    return render(request, 'product/index.html', {'loaisp':ds, 'sanpham':sp})
+    return render(request, 'product/index.html', {'loaisp':ds})
 
 def detail(request, id):
     
@@ -265,5 +264,32 @@ def contact(request):
     return render(request,"product/contact.html")
 def search(request):
     q=request.GET.get('q')
-    sp=Product.objects.filter(title__icontains=q).order_by('-id')
+    query=q.split()
+    print(query)
+    # if len(query)==1:
+    #     sp=Product.objects.filter(title__icontains=query[0])
+    # elif len(query)==2:
+    #     sp=Product.objects.filter(Q(title__icontains=query[0]) | 
+    #     Q(title__icontains=query[1]) )
+    # elif len(query)==3:
+    #     sp=Product.objects.filter(Q(title__icontains=query[0]) | 
+    #     Q(title__icontains=query[1]) | 
+    #     Q(title__icontains=query[2]) )
+    # elif len(query)==4:
+    #     sp=Product.objects.filter(Q(title__icontains=query[0]) | 
+    #     Q(title__icontains=query[1]) | 
+    #     Q(title__icontains=query[2])| 
+    #     Q(title__icontains=query[3]) )
+    # elif len(query)==5:
+    #     sp=Product.objects.filter(Q(title__icontains=query[0]) | 
+    #     Q(title__icontains=query[1]) | 
+    #     Q(title__icontains=query[2]) | 
+    #     Q(title__icontains=query[3]) | 
+    #     Q(title__icontains=query[4]) 
+    #     ) 
+    queries = [Q(title__icontains=query) for query in query]
+    query1 = queries.pop()
+    for item in queries:
+        query1 |= item
+    sp = Product.objects.filter(query1)
     return render(request,"product/search.html",{'sp':sp})
